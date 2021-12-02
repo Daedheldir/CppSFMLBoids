b5df6eef450d16e196b898555b2757efadb65ad2
Daedheldir <marcin.bernacki316@gmail.com>
2021-12-02 17:11:16 +0100

    Revert "- placed everything into respective folders"
    This reverts commit 17ba48cd94b132b3dd6e9c052faabe76c456b5b4.

--- SFMLAdvEngine/main.cpp
+++ SFMLAdvEngine/main.cpp
@@ -12,25 +12,25 @@ protected:
 	dh::GameDataRef m_gameData;
 	void loadResources() {};
 	void handleEvents(sf::Event& ev) {
-		if ((ev.type == sf::Event::KeyPressed && ev.key.code == sf::Keyboard::Escape) || ev.type == sf::Event::Closed)
-			m_gameData->bGameRunning = false;
+		if ((ev.type == sf::Event::KeyPressed && ev.key.code == sf::Keyboard::Escape) || ev.type == sf::Event::Closed) 
+			m_gameData->bGameRunning = false; 
 	};
 	void handleInput() {};
 	void handleLogic() { flock.Update(); };
-	void handleDrawing()
-	{
-		getGraphics().getRenderWindow().clear();
-		getGraphics().getRenderWindow().draw(flock.boidsVerticesArr);
-		getGraphics().getRenderWindow().display();
+	void handleDrawing() 
+	{ 
+		getGraphics().getRenderWindow().clear(); 
+		getGraphics().getRenderWindow().draw(flock.boidsVerticesArr); 
+		getGraphics().getRenderWindow().display(); 
 	};
 
 private:
 	BoidFlock flock;
-
+	
 };
 
 int main() {
-	srand(0);	//seeding PRNG with constant value so all calculations are deterministic
+	srand(0);
 	dh::GameDataRef m_gameData = std::make_shared<dh::GameData>();
 	BoidsApp boidsApp(m_gameData);
 	boidsApp.Run();
